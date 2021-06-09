97e469a4e5015f8893c3e6cb1566a32edb5f8bef
Dibyanshu Mohanty <dibyanshu.m2002@gmail.com>
2021-06-09 21:49:15 +0530

    Update settings.py

--- Robo/settings.py
+++ Robo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c64fe_s$(ui_49h=yxhzjw+hzs1u6k5ik!*)^*twg%%inghlr+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -144,6 +144,6 @@ MESSAGE_TAGS = {
 # EMAIL
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'dibyanshu.m2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'wvwrisabkxsbblbn'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
